6a3ef74b8f7aa1d96705069849160e132757c2ee
Midoriin <fbyigit07@hotmail.com>
2021-04-21 23:52:32 +0300

    handle overhaul

--- process_memory_manager.py
+++ process_memory_manager.py
@@ -1,4 +1,5 @@
 import ctypes
+import functools
 import logging
 import time
 from typing import List
@@ -13,18 +14,22 @@ import key_manager
 
 
 class Process:
-    def __init__(self, process_id: int, process_name: str, process_handle, base_address):
+    def __init__(self, process_id: int, process_name: str, base_address):
         self.process_id = process_id
         self.process_name = process_name
 
         self.base_address = base_address
 
-        self._process_handle = process_handle
-        self._window_handle = None
         self.__last_window_handle = None
 
-    def __del__(self):
-        win32api.CloseHandle(self._process_handle)
+    @functools.cached_property
+    def process_handle(self):
+        return ctypes.windll.kernel32.OpenProcess(win32con.PROCESS_VM_READ, False, self.process_id)
+
+    @functools.cached_property
+    def window_handle(self):
+        title = self.process_name.split('.')[0].title()
+        return win32gui.FindWindow(None, title)
 
     @classmethod
     def get_by_name(cls, process_name: str) -> "Process":
@@ -36,22 +41,28 @@ class Process:
             if process_id == -1:
                 continue
 
+            handle = None
             # Try to read the process memory
             try:
                 handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ,
                                               True, process_id)
             except:
                 continue
+            else:
+                # iterate over an array of base addresses of each module
+                for base_address in win32process.EnumProcessModules(handle):
+                    # Get the name of the module
+                    current_name = str(win32process.GetModuleFileNameEx(handle, base_address))
 
-            # iterate over an array of base addresses of each module
-            for base_address in win32process.EnumProcessModules(handle):
-                # Get the name of the module
-                current_name = str(win32process.GetModuleFileNameEx(handle, base_address))
+                    # compare it
+                    if process_name.casefold() in current_name.casefold():
+                        logging.debug(f"Base address of {process_name} ({process_id}): {hex(base_address)}")
+                        return cls(process_id, process_name, base_address)
 
-                # Compare it to the name of your program
-                if process_name.casefold() in current_name.casefold():
-                    logging.debug(f"Base address of {process_name} ({process_id}): {hex(base_address)}")
-                    return cls(process_id, process_name, handle, base_address)
+            finally:
+                if handle:
+                    # close the handle as we don't need it anymore
+                    win32api.CloseHandle(handle)
 
         raise Exception(f"{process_name} could not be found.")
 
@@ -59,83 +70,54 @@ class Process:
         """Read a process' memory based on its process id, address and offsets.
         Returns the address without offsets and the value."""
 
-        logging.debug(f"Address of the memory that's going to be read: {hex(address)}")
-
-        # The handle to the program's process
-        # This will allow to use ReadProcessMemory
-        h_process = ctypes.windll.kernel32.OpenProcess(win32con.PROCESS_VM_READ, False, self.process_id)
-
-        # This is a pointer to the data you want to read
-        # Use `data.value` to get the value at this pointer
-        # In this case, this value is an Integer with 4 bytes
+        # Pointer to the data want to read
         data = ctypes.c_uint(0)
 
-        # Size of the variable, it usually is 4 bytes
+        # Size of the variable, 4 bytes
         bytes_read = ctypes.c_uint(0)
 
         # Starting address
         current_address = address
 
         if offsets:
-            # Append a new element to the offsets array
-            # This will allow you to get the value at the last offset
-            offsets.append(None)
-
             for offset in offsets:
                 # Convert to int if its str
                 if isinstance(offset, str):
                     offset = int(offset, 16)
 
                 # Read the memory of current address using ReadProcessMemory
-                ctypes.windll.kernel32.ReadProcessMemory(h_process, current_address, ctypes.byref(data),
+                ctypes.windll.kernel32.ReadProcessMemory(self.process_handle, current_address, ctypes.byref(data),
                                                          ctypes.sizeof(data), ctypes.byref(bytes_read))
 
-                # If current offset is `None`, return the value of the last offset
-                if not offset:
-                    logging.debug(f"(Address: {hex(current_address)}) Value: {data.value}")
-                    return current_address, data.value
-                else:
-                    # Replace the address with the new data address
-                    current_address = data.value + offset
-
+                # Replace the address with the new data address
+                current_address = data.value + offset
         else:
             # Just read the single memory address
-            ctypes.windll.kernel32.ReadProcessMemory(h_process, current_address, ctypes.byref(data),
+            ctypes.windll.kernel32.ReadProcessMemory(self.process_handle, current_address, ctypes.byref(data),
                                                      ctypes.sizeof(data),
                                                      ctypes.byref(bytes_read))
 
-        # Close the handle to the process
-        # ctypes.windll.kernel32.CloseHandle(h_process)
-
         # Return a pointer to the value and the value
-        # The pointer will be used to write to the memory
+        # If current offset is `None`, return the value of the last offset
+        logging.debug(f"(Address: {hex(current_address)}) Value: {data.value}")
         return current_address, data.value
-
-    # def _window_enum_handler(self, window_handle, process_name):
-    #     if win32gui.GetWindowText(window_handle).casefold() == process_name:
-    #         self._window_handle = window_handle
-
-    @property
-    def window_handle(self):
-        if self._window_handle is None:
-            title = self.process_name.split('.')[0].title()
-            self._window_handle = win32gui.FindWindow(None, title)
-
-        return self._window_handle
 
     def focus(self):
         self.__last_window_handle = win32gui.GetForegroundWindow()
 
         if self.__last_window_handle != self.window_handle:
-            # send an alt key first to make this work
-            key_manager.Presser.press_and_release('alt', sleep_between=0)
-
             exc = None
             # try 2 times
             for _ in range(2):
                 try:
+                    # send an alt key first to make this work
+                    key_manager.Presser.press_and_release('alt', sleep_between=0)
                     win32gui.SetForegroundWindow(self.window_handle)
+
                 except Exception as e:
+                    # del the attribute to update it
+                    delattr(self, 'window_handle')
+
                     exc = e
                     time.sleep(0.1)
                 else:
@@ -153,12 +135,25 @@ class Process:
 
     @staticmethod
     def kill_by_name(names: List[str]):
+        names = list(map(lambda x: x.casefold(), names))
+
         for proc in psutil.process_iter():
-            if proc.name() in names:
+            if proc.name().casefold() in names:
                 try:
                     proc.kill()
                 except psutil.AccessDenied:
                     logging.debug(f'Could not kill process "{proc.name()}". Ignoring.')
+                except psutil.NoSuchProcess:
+                    pass
+
+    def __del__(self):
+        if self.process_handle:
+            # close the handle
+            ctypes.windll.kernel32.CloseHandle(self.process_handle)
+
+    # def _window_enum_handler(self, window_handle, process_name):
+    #     if win32gui.GetWindowText(window_handle).casefold() == process_name:
+    #         self._window_handle = window_handle
 
     # @staticmethod
     # def char2key(c):
